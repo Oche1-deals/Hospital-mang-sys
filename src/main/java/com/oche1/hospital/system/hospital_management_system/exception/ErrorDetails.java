c87c84f5f0cda6b5b4bee461c427d9a1270202d2
Oche1-deals <odohraphaeloche11@gmail.com>
2024-11-16 16:41:25 +0100

    second commit

---
+++ src/main/java/com/oche1/hospital/system/hospital_management_system/exception/ErrorDetails.java
@@ -0,0 +1,34 @@
+package com.oche1.hospital.system.hospital_management_system.exception;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+    private int status;
+
+    public ErrorDetails(LocalDateTime timestamp, String message, String details, int status) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+        this.status = status;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
--- src/main/java/com/oche1/hospital/system/hospital_management_system/patients/PatientResource.java
+++ src/main/java/com/oche1/hospital/system/hospital_management_system/patients/PatientResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PatientResource {
@@ -17,17 +18,27 @@ public class PatientResource {
         this.repository = repository;
     }
     @GetMapping("patients")
-    public List<Patient> retriveAllUser(){
+    public List<Patient> retriveAllPatients(){
         return repository.findAll();
+    }
+    @GetMapping("patients/{id}")
+    public Patient retrivePatient(@PathVariable Integer id){
+        Patient patient = repository.findById(id).orElse(null);
+        if(patient== null)
+            throw new UserNotFoundException("id:"+id);
+
+        return patient;
     }
     @DeleteMapping("/patients/{id}")
     public void deletePatient(@PathVariable int id){
         repository.deleteById(id);
     }
     @PostMapping("/patients")
-    public ResponseEntity<Patient> createUser(@Valid @RequestBody Patient patient){
-        Patient savedUser = repository.save(patient);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+    public ResponseEntity<Patient> createPatient(@Valid @RequestBody Patient patient){
+        Patient savedPatient = repository.save(patient);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}").buildAndExpand(savedPatient.getId()).toUri();
         return ResponseEntity.created(location).build();
     }
 
